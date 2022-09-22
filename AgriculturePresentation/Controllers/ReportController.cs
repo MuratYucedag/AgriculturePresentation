baab315ff1a64529ff8fafe5cf253736bfd6f31d
MuratYucedag <MurattYucedag@users.noreply.github.com>
2022-09-22 13:40:32 +0300

    Ders 151 sonu

---
+++ AgriculturePresentation/Controllers/ReportController.cs
@@ -0,0 +1,94 @@
+﻿using AgriculturePresentation.Models;
+using ClosedXML.Excel;
+using DataAccessLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgriculturePresentation.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StaticReport()
+        {
+            ExcelPackage excelPackage = new ExcelPackage();
+            var workBook = excelPackage.Workbook.Worksheets.Add("Dosya1");
+
+            workBook.Cells[1, 1].Value = "Ürün Adı";
+            workBook.Cells[1, 2].Value = "Ürün Kategorisi";
+            workBook.Cells[1, 3].Value = "Ürün Stok";
+
+            workBook.Cells[2, 1].Value = "Mercimek";
+            workBook.Cells[2, 2].Value = "Bakliyat";
+            workBook.Cells[2, 3].Value = "785 Kg";
+
+            workBook.Cells[3, 1].Value = "Buğday";
+            workBook.Cells[3, 2].Value = "Bakliyat";
+            workBook.Cells[3, 3].Value = "1.986 Kg";
+
+            workBook.Cells[4, 1].Value = "Havuç";
+            workBook.Cells[4, 2].Value = "Sebze";
+            workBook.Cells[4, 3].Value = "167 Kg";
+
+            var bytes = excelPackage.GetAsByteArray();
+            return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "BakliyatRaporu.xlsx");
+        }
+
+
+        public List<ContactModel> ContactList()
+        {
+            List<ContactModel> contactModels = new List<ContactModel>();
+            using (var context = new AgricultureContext())
+            {
+                contactModels = context.Contacts.Select(x => new ContactModel
+                {
+                    ContactID = x.ContactID,
+                    ContactName = x.Name,
+                    ContactDate = x.Date,
+                    ContactMail = x.Mail,
+                    ContactMessage = x.Message
+                }).ToList();
+            }
+            return contactModels;
+        }
+        public IActionResult ContactReport()
+        {
+            using (var workBook = new XLWorkbook())
+            {
+                var workSheet = workBook.Worksheets.Add("Mesaj Listesi");
+                workSheet.Cell(1, 1).Value = "Mesaj ID";
+                workSheet.Cell(1, 2).Value = "Mesaj Gönderen";
+                workSheet.Cell(1, 3).Value = "Mail Adresi";
+                workSheet.Cell(1, 4).Value = "Mesaj İçeriği";
+                workSheet.Cell(1, 5).Value = "Mesaj Tarihi";
+
+                int contactRowCount = 2;
+                foreach (var item in ContactList())
+                {
+                    workSheet.Cell(contactRowCount, 1).Value = item.ContactID;
+                    workSheet.Cell(contactRowCount, 2).Value = item.ContactName;
+                    workSheet.Cell(contactRowCount, 3).Value = item.ContactMail;
+                    workSheet.Cell(contactRowCount, 4).Value = item.ContactMessage;
+                    workSheet.Cell(contactRowCount, 5).Value = item.ContactDate;
+                    contactRowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workBook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "pplication/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "MesajRapor.xlsx");
+                }
+            }
+        }
+    }
+}
---
+++ AgriculturePresentation/Models/ContactModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgriculturePresentation.Models
+{
+    public class ContactModel
+    {
+        public int ContactID { get; set; }
+        public string ContactName { get; set; }
+        public string ContactMail { get; set; }
+        public string ContactMessage { get; set; }
+        public DateTime ContactDate { get; set; }
+    }
+}
